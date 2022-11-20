7264f672752098332f6c06bf394a23ba3c222e42
Ary N <arynoviyanto@gmail.com>
2022-11-20 23:56:09 +1300

    Fix when installing tailwindcss

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,5 @@
 [ -f "./product/package.json" ] || (yes | npm create vite@latest product-tmp -- --template react \
     && mv product-tmp/* product/ \
     && rm -r product-tmp/ \
-    && cd product/ && npm install -D tailwindcss postcss autoprefixer && npx tailwindcss init
+    && cd product/ && npm install -D tailwindcss postcss autoprefixer && npx tailwindcss init -p
 )
