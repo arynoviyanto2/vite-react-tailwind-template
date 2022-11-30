7830107d0f1c1afb8b72927513d238657e218b49
Ary N <arynoviyanto@gmail.com>
2022-12-01 01:32:33 +1300

    Update startup.sh
    add .gitignore

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,6 @@
 [ -f "./product/package.json" ] || (yes | npm create vite@latest product-tmp -- --template react \
     && mv product-tmp/* product/ \
     && rm -r product-tmp/ \
-    && cd product/ && npm install -D tailwindcss postcss autoprefixer && npx tailwindcss init -p
+    && cd product/ && npm install -D tailwindcss postcss autoprefixer && npx tailwindcss init -p \
+	&& printf 'node_modules\ndist' > .gitignore
 )
